1de13cd6ba3bcd575c802aed71368d4345675fc1
Mike Seith Williams <mike-seith.williams@tu-dortmund.de>
2025-02-18 16:47:46 +0100

    Init Backend

---
+++ airbnb-clone-back/src/test/java/de/seith_codecake/airbnb_clone_back/AirbnbCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package de.seith_codecake.airbnb_clone_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
